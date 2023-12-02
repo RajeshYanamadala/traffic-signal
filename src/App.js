27015367215333d1232fc49256ab1b035439787d
Rajesh <rajesh2012y@gmail.com>
2023-12-02 12:17:59 +0530

    new implemnet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { ColorRing } from "react-loader-spinner";
 
-function App() {
+const TrafficSignal = ({ color }) => {
+  return <div className={`traffic-light ${color}`} />;
+};
+
+const App = () => {
+  const [red, setRed] = useState("");
+  const [yellow, setYellow] = useState("");
+  const [green, setGreen] = useState("");
+  const [show, setShow] = useState(false);
+
+  // let interval;
+
+  useEffect(() => {
+    timeStart();
+  }, []);
+
+  const timeStart = () => {
+    setTimeout(() => {
+      setRed("red");
+    }, 3000);
+  };
+
+  if (red === "red") {
+    setTimeout(() => {
+      setYellow("yellow");
+    }, 3000);
+  }
+
+  if (yellow === "yellow") {
+    setTimeout(() => {
+      setGreen("green");
+      setShow(true);
+    }, 3000);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Traffic Signal App</h1>
+      <div className="traffic-signals">
+        {red == "red" ? (
+          <TrafficSignal color={`${red === "red" ? "red" : "grey"}`} />
+        ) : (
+          <ColorRing visible={true} height="80" width="80" />
+        )}
+
+        <TrafficSignal color={`${yellow === "yellow" ? "yellow" : "grey"}`} />
+
+        <TrafficSignal color={`${green === "green" ? "green" : "grey"}`} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
